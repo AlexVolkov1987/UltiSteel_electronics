a3cf91b5541e13c3f6782a9aa160e06cba69be91
AlexVolkov1987 <a.volkov1987@gmail.com>
2021-05-27 11:52:06 +0300

    add firmware folder
    Init with atmel studio

---
+++ Advanced_power_control/Firmware/APC/APC/APC.c
@@ -0,0 +1,20 @@
+/*
+ * APC.c
+ *
+ * Created: 27.05.2021 11:39:06
+ *  Author: alexd
+ */ 
+
+
+#include <avr/io.h>
+#include "iom164pa.h"
+
+int main(void)
+{
+    PORTA = 0x00;
+	
+	while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
